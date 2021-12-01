fde457a6f9d375869fbea63161e359cf65c6eef5
Kathelyn <kathy.lima.mene@gmail.com>
2021-12-01 02:42:16 +0000

    react-hooks

---
+++ hooks/src/views/contents/NaoEncontrado.jsx
@@ -0,0 +1,11 @@
+import './NaoEncontrado.css'
+import React from "react"
+
+const NaoEncontrado = props => (
+    <div className="erro">
+        <h1><u>Página não encontrada.</u> </h1>
+    </div>
+
+)
+
+export default NaoEncontrado
